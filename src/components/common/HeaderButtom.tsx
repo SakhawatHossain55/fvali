506b3f45adee9f14390b9e451b1f9bec1efb3e03
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-08-17 11:07:01 +0600

    add header buttom

--- src/components/common/HeaderButtom.tsx
+++ src/components/common/HeaderButtom.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const HeaderButtom = () => {
-  return <div>Header Buttom</div>;
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Categories</Navbar.Brand>
+        <Nav className="ms-auto">
+          <Nav.Link className="active" href="#home">
+            News feed
+          </Nav.Link>
+          <Nav.Link className="active" href="#features">
+            Merchant Zone
+          </Nav.Link>
+          <Nav.Link className="active" href="#pricing">
+            Help
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default HeaderButtom;
