3f7388db92949e79b5636b249148fd4b02921f0f
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-08-24 17:35:34 +0600

    [ADD SHOPBYSTORE]

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,11 @@
 import Banner from "components/home/Banner/Banner";
 import Products from "components/home/Products/Products";
+import ShopByStore from "components/home/ShopByStore";
 import useAsync from "hooks/useAsync";
-// import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProductService from "services/ProductService";
+import StoreService from "services/StoreService";
+import { IStore } from "types";
 // import { IProduct } from "types";
 
 const Home = () => {
@@ -12,6 +15,12 @@ const Home = () => {
   //   ProductService.getProducts().then((res) => setProducts(res));
   // }, []);
 
+  const [storeData, setStoreData] = useState<IStore[]>([]);
+  console.log(storeData);
+  useEffect(() => {
+    StoreService.getStores().then((res) => setStoreData(res));
+  }, []);
+
   const { data, isLoading, isSuccess } = useAsync(ProductService.getProducts);
   console.log(data);
 
@@ -19,6 +28,11 @@ const Home = () => {
     <div>
       <Banner />
       <Products isSuccess={isSuccess} isLoading={isLoading} products={data} />
+      <ShopByStore
+        isSuccess={isSuccess}
+        isLoading={isLoading}
+        shopByStore={storeData}
+      />
     </div>
   );
 };
